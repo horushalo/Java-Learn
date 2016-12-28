0fd5581f6922544353970324c091886fe3b2b70d
Zaslavskiy.G <Zaslavskiy.G@ADM-ZASLAVSK.partner.ru>
2016-12-28 16:58:00 +1000

    1

--- MyCalculator/src/Calculator.java
+++ MyCalculator/src/Calculator.java
@@ -18,7 +18,14 @@ public class Calculator {
 	JButton buttonPoint;
 	
 	JButton buttonEqual;
+	
+	JButton buttonPlus;
+	JButton buttonMinus;
+	JButton buttonDivide;
+	JButton buttonMultiply;
+	
 	JPanel p1;
+	JPanel p2;
 	
 		Calculator() {
 			
@@ -27,8 +34,12 @@ public class Calculator {
 			BorderLayout bl = new BorderLayout();
 			windowContent.setLayout(bl);
 			
-			displayField = new JTextField(30);
+			//displayField = new JTextField(20);
+			displayField = new JFormattedTextField();
+			displayField.setHorizontalAlignment(SwingConstants.RIGHT);
 			windowContent.add("North", displayField);
+			
+			
 			
 			button0 = new JButton("0");
 			button1 = new JButton("1");
@@ -40,11 +51,17 @@ public class Calculator {
 			button7 = new JButton("7");
 			button8 = new JButton("8");
 			button9 = new JButton("9");
+			
+			
 			buttonPoint = new JButton(".");
 			buttonEqual = new JButton("=");
+			buttonPlus = new JButton("+");
+			buttonMinus = new JButton("-");
+			buttonDivide = new JButton("/");
+			buttonMultiply = new JButton("*");
 			
 			p1 = new JPanel();
-			GridLayout gl = new GridLayout(4, 3);
+			GridLayout gl = new GridLayout(4,3);
 			p1.setLayout(gl);
 			
 			p1.add(button0);
@@ -60,7 +77,21 @@ public class Calculator {
 			p1.add(buttonPoint);
 			p1.add(buttonEqual);
 			
+			
+			p2 = new JPanel();
+			GridLayout gl2 = new GridLayout(4, 1);
+			p2.setLayout(gl2);
+			
+			p2.add(buttonPlus);
+			p2.add(buttonMinus);
+			p2.add(buttonDivide);
+			p2.add(buttonMultiply);
+			
+			
+			
+			
 			windowContent.add("Center", p1);
+			windowContent.add("East", p2);
 			JFrame frame = new JFrame("Calculator");
 			frame.setContentPane(windowContent);
 			
